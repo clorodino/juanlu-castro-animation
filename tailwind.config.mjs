4acdbf2e5d0652bd551e0aa29859292b606ee000
Juanlu Castro <juanluiscas@gmail.com>
2025-01-20 11:07:12 +0100

    chore: add some pages

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,7 +3,22 @@ export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   darkMode: "class",
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        "text-reveal": "text-reveal 1.5s cubic-bezier(0.77, 0, 0.175, 1) 0.5s",
+      },
+      keyframes: {
+        "text-reveal": {
+          "0%": {
+            transform: "translate(0, 100%)",
+          },
+          "100%": {
+            transform: "translate(0, 0)",
+          },
+        },
+      },
+    },
   },
   plugins: [],
 };
+
