4432f383025bfedba55632b56d239ec2afb7ab0f
Juanlu Castro <juanluiscas@gmail.com>
2025-01-21 12:49:45 +0100

    chore: update projects

--- src/pages/api/data.json.ts
+++ src/pages/api/data.json.ts
@@ -10,6 +10,17 @@ export interface dataItem {
   videos?: string | string[];
   links?: string | string[];
   description: string | string[];
+  credits?: {
+    direction?: string | string[];
+    design?: string | string[];
+    animation?: string | string[];
+    illustration?: string | string[];
+    music?: string | string[];
+    sound?: string | string[];
+    client?: string | string[];
+    cleanUp?: string | string[];
+    story?: string | string[];
+  };
 }
 
 const data: dataItem[] = [
@@ -17,7 +28,12 @@ const data: dataItem[] = [
     title: "JC Ident",
     url: "/",
     images: { port: "src/assets/img/Reel_Port-1.gif", projectImages: [] },
-    description: "",
+    description: "This video is an introduction for my showreel. I have included part of the process from storyboard to final clip.",
+    credits: {
+      direction: "Juanlu Castro",
+      design: "Juanlu Castro",
+      animation: "Juanlu Castro",
+    },
   },
   {
     title: "Graf 2018",
@@ -51,9 +67,27 @@ const data: dataItem[] = [
   },
   {
     title: "Your planet sucks",
-    url: "/",
+    url: "/projects/your-planet-sucks",
     images: { port: "src/assets/img/00-YPS_port.gif", projectImages: [] },
-    description: [""],
+    description: [`Your Planet Sucks is a creative studio based in Barcelona focused on 3D illustration and animation, and works on odd and different projects. I've made a few little animations for the upcoming website.`],
+    credits: {
+      direction: ["Juanlu Castro & Antonio Hitos"],
+      design: "Juanlu Castro",
+      animation: "Juanlu Castro",
+     
+    },
+  },
+  {
+    title: "Weird Walk",
+    url: "/projects/weird-walk",
+    images: { port: "src/assets/img/00-YPS_port.gif", projectImages: [] },
+    description: [`Weird Walk is a piece made from several loops posted on Instagram. The main intention was to make an "exquisite corpse", posting a clip based on the previous every two days.`, `Finally, I made the entire composition with all the clips, creating a big loop.`],
+    credits: { 
+      direction: "Juanlu Castro",
+      design: "Juanlu Castro",
+      animation: "Juanlu Castro",
+      sound: "Juanlu Castro",
+    },
   },
 ];
 
